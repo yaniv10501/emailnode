21f09dec89f064257e2a05e36ccedcd4d90ffa4f
Yaniv <yaniv10501@gmail.com>
2021-12-29 15:49:26 +0000

    ,

--- utils/cors.js
+++ utils/cors.js
@@ -1,12 +1,12 @@
 const AuthorizationError = require('./errors/AuthorizationError');
 
-const whiteList = ['https://yanivportfolio.com', 'https://www.yanivportfolio.com'];
+const whiteList = ['https://yanivportfolio.com/', 'https://www.yanivportfolio.com/'];
 const corsOptions = {
   origin: (origin, callback) => {
     if (whiteList.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new AuthorizationError('Not allowed by CORS'));
+      callback(new AuthorizationError('Origin - ' + origin + ' is not allowed by CORS'));
     }
   },
   optionsSuccessStatus: 200,
