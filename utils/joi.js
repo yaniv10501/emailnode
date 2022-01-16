cf8b7edcbad7d60c5a6f19d98aa21f26018dc914
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-01-16 17:45:28 +0200

    /

--- utils/joi.js
+++ utils/joi.js
@@ -39,7 +39,7 @@ const sendEmailSchema = Joi.object({
   from: Joi.string().custom(emailMethod).required(),
   name: Joi.string().custom(nameMethod).required(),
   text: Joi.string().custom(textMethod).required(),
-  phoneNumber: Joi.string().custom(phoneNumberMethod).default(''),
+  phoneNumber: Joi.string().custom(phoneNumberMethod).allow(''),
 });
 
 module.exports.checkEmailReq = (req, res, next) => {
