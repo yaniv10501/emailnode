a6299fd15cba20565ec3bf655583a09526d4d617
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-09-09 21:36:29 +0300

    secure mail

--- controllers/email.js
+++ controllers/email.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 const logger = require("../utils/logger");
 
-module.exports.sendEmail = (req, res, next) => {
+module.exports.sendEmail = async (req, res, next) => {
   const { MAIL_PROVIDER, MAIL_NAME, MAIL_PASS, MAIL_TO } = process.env;
   const { from, name, text, phoneNumber } = req.body;
   try {
     const transporter = nodemailer.createTransport({
       service: MAIL_PROVIDER,
+      port: 465,
+      secure: true,
+      tls: true,
       auth: {
         user: MAIL_NAME,
         pass: MAIL_PASS,
